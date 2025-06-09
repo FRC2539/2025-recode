b3b54087f1a6163f24cebf8ea54deb633b69b063
AlexNunemacher <apullinune@gmail.com>
2025-06-09 18:28:49 -0400

    io

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -1,5 +1,23 @@
 package frc.robot.subsystems.arm;
 
-public class ArmIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ArmIO {
+
+    public void updateInputs(ArmIOInputs inputs);
+
+    @AutoLog
+    public class ArmIOInputs {
+        public double position = 0.0; 
+        public double voltage = 0.0; 
+        public double velocity = 0.0; 
+        public double current = 0.0;
+        public double temperature = 0.0;
+        public boolean throughboreEncoderConnection = false;
+        public double throughboreEncoderPos = 0.0;
+    }
+
+    public void setVoltage(double voltage);
+    public void setPosition(double position);
     
-}
+} 
