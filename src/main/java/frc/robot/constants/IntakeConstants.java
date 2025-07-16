c74fc5b81fe9523f7b87ae71952cfa9b8ada8ea5
Asa Wang <axxwang13@gmail.com>
2025-07-16 19:20:15 -0400

    preliminary ground intake code
    Co-authored-by: AlexNunemacher <AlexNunemacher@users.noreply.github.com>
    Co-authored-by: James  <jamesgormleyrechs@gmail.com>

---
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.constants;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+
+public class IntakeConstants {
+    public static final int pivotMotorId = 100;
+    public static final String pivotMotorCanBus = "";
+    public static final CurrentLimitsConfigs pivotCurrentLimit = new CurrentLimitsConfigs();
+
+    public static final int wheelsMotorId = 100;
+    public static final String wheelsMotorCanBus = "";
+    public static final CurrentLimitsConfigs wheelsCurrentLimit = new CurrentLimitsConfigs();
+
+    
+}
---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    
+    public void updateInputs(IntakeIOInputs inputs);
+
+    @AutoLog
+    public class IntakeIOInputs {
+        double pivotVoltage = 0;
+        double pivotPosition = 0;
+
+        double wheelsVoltage = 0;
+    }
+
+    public void setWheelsVoltage(double voltage);
+
+    public void setPivotPosition(double position);
+
+}
